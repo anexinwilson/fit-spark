2f9b66994ff884ed0c057d66a6f6f128ded505e9
anexinwilson <anexin777@gmail.com>
2025-07-06 03:12:00 -0700

    add configuration for auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,18 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { Roboto } from "next/font/google";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
+import "./globals.css";
 import NavBar from "@/components/navbar";
+import { Geist, Geist_Mono } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "@mui/material";
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
@@ -11,19 +22,49 @@ const roboto = Roboto({
   variable: "--font-roboto",
 });
 
-interface RootLayoutProps {
-  children: React.ReactNode;
-}
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-export default function RootLayout(props: RootLayoutProps) {
-  const { children } = props;
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="en" className={roboto.variable}>
-      <body>
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}><NavBar/>{children}</ThemeProvider>
-        </AppRouterCacheProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html
+        lang="en"
+        className={`${roboto.variable} ${geistSans.variable} ${geistMono.variable}`}
+      >
+        <body className="antialiased">
+          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+            <ThemeProvider theme={theme}>
+              <header className="flex justify-end items-center p-4 gap-4 h-16">
+                <SignedOut>
+                  <SignInButton>
+                    <Button variant="outlined">Sign In</Button>
+                  </SignInButton>
+                  <SignUpButton>
+                    <Button variant="contained">Sign Up</Button>
+                  </SignUpButton>
+                </SignedOut>
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+              </header>
+              <NavBar />
+              {children}
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,44 @@
-import Image from "next/image"
-import Link from "next/link"
+"use client";
+import { SignedIn, SignedOut, SignOutButton, useUser } from "@clerk/nextjs";
+import { Button } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
 
 const NavBar = () => {
-    return  (
-        <nav>
-        {" "}
-        <div>
-            <Link href="/">
-            <Image src = "/window.svg" width={60} height={60} alt="Logo"/>
+  const { isLoaded, isSignedIn, user } = useUser();
+  if (!isLoaded) {
+    return <p>Loading ...</p>;
+  }
+  return (
+    <nav>
+      <div>
+        <Link href="/">
+          <Image src="/window.svg" width={60} height={60} alt="Logo" />
+        </Link>
+      </div>
+      <div>
+        <SignedIn>
+          <Link href="/workoutplan">Workout Plan</Link>
+          {user?.imageUrl ? (
+            <Link href="/profile">
+              <Image
+                src={user.imageUrl}
+                alt="Profile Picture"
+                width={40}
+                height={40}
+              />
             </Link>
-        </div>
-        <div>
+          ) : (
+            <div></div>
+          )}
+          <SignOutButton>
+            <Button>Sign Out</Button>
+          </SignOutButton>
+        </SignedIn>
+        <SignedOut></SignedOut>
+      </div>
+    </nav>
+  );
+};
 
-        </div>
-        </nav>
-    )
-}
-
- export default NavBar
+export default NavBar;
